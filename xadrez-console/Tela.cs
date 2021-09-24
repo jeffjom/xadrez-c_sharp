a542929fe593a54be22069e39804dd5fdbdf5ab8
Jefferson <jeff_jom@hotmail.com>
2021-09-23 23:49:24 -0300

    Acrescentando as demais pecas

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -51,7 +51,7 @@ namespace xadrez_console {
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("  A B C D E F G H");
+            Console.WriteLine("  a b c d e f g h");
         }
 
         public static void imprimiTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis) {
